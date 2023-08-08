c5c719e30d42447ab42e03f9b3e82d81742a90ba
shuvakarmakar <shuva15-7953@diu.edu.bd>
2023-08-08 12:49:33 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+require("dotenv").config();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// Ping Endpoint
+app.get("/", (req, res) => {
+    res.send("Spoken English Server is running");
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
