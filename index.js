dd1b179a4c6cc22750c8a0ec44bb2a242073251e
shuvakarmakar <shuva15-7953@diu.edu.bd>
2023-08-21 20:38:36 +0600

    Added POST API for SSL Order Data

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use(
-  cors({
-    origin: "http://localhost:5173", // Replace with your frontend's domain
-  })
+    cors({
+        origin: "http://localhost:5173", // Replace with your frontend's domain
+    })
 );
 // Ping Endpoint
 app.get("/", (req, res) => {
@@ -70,7 +70,7 @@ const store_id = process.env.STORE_ID
 const store_passwd = process.env.STORE_PASS
 const is_live = false //true for live, false for sandbox
 
-console.log(store_id,store_passwd);
+console.log(store_id, store_passwd);
 
 async function run() {
     try {
@@ -91,6 +91,7 @@ async function run() {
         const blogsCollection = client.db("Spoken-English").collection("blogs");
         const coursesCollection = client.db("Spoken-English").collection("Courses");
         const userCollection = client.db("Spoken-English").collection("users");
+        const orderCollection = client.db("Spoken-English").collection("orders");
 
 
         // For Transection Id of SSL Commerze
@@ -99,52 +100,78 @@ async function run() {
         // APIs
 
         // FOr Payment Gateway
-      
+
 
         app.post("/order", async (req, res) => {
             const order = req.body;
             // const courseDetail = await coursesCollection.findOne({ _id: new ObjectId(courseId) });
-          console.log(order.price);
+            // console.log(order.price);
             const data = {
-              total_amount: order.price,
-               currency: 'BDT',
-              tran_id: tran_id, // use unique tran_id for each api call
-              success_url: "http://localhost:5173/success",
-              fail_url: "http://localhost:5173/fail",
-              cancel_url: "http://localhost:5173/cancel",
-              ipn_url: "http://localhost:5173/ipn",
-              shipping_method: "Courier",
-              product_name: order.course_name,
-              product_category: "Educational",
-              product_profile: "general",
-              cus_name: order.billingData.fullName,
-              cus_email: order.billingData.email,
-              cus_add1: order.billingData.address,
-              cus_city: order.billingData.city,
-              cus_country: order.billingData.country,
-              cus_postcode: order.billingData.postalCode,
-              cus_phone: order.billingData.contactNumber,
-               cus_fax: '01711111111',
-              ship_name: "Customer Name",
-              ship_add1: "Dhaka",
-              ship_add2: "Dhaka",
-              ship_city: "Dhaka",
-              ship_state: "Dhaka",
-              ship_postcode: 1000,
-              ship_country: "Bangladesh",
+                total_amount: order.price,
+                currency: 'BDT',
+                tran_id: tran_id, // use unique tran_id for each api call
+                success_url: `http://localhost:5000/payment/success/${tran_id}`,
+                fail_url: "http://localhost:5173/fail",
+                cancel_url: "http://localhost:5173/cancel",
+                ipn_url: "http://localhost:5173/ipn",
+                shipping_method: "Courier",
+                product_name: order.course_name,
+                product_category: "Educational",
+                product_profile: "general",
+                cus_name: order.billingData.fullName,
+                cus_email: order.billingData.email,
+                cus_add1: order.billingData.address,
+                cus_city: order.billingData.city,
+                cus_country: order.billingData.country,
+                cus_postcode: order.billingData.postalCode,
+                cus_phone: order.billingData.contactNumber,
+                cus_fax: '01711111111',
+                ship_name: "Customer Name",
+                ship_add1: "Dhaka",
+                ship_add2: "Dhaka",
+                ship_city: "Dhaka",
+                ship_state: "Dhaka",
+                ship_postcode: 1000,
+                ship_country: "Bangladesh",
             };
 
-            console.log(data);
+            // console.log(data);
             const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
             sslcz.init(data).then(apiResponse => {
                 // Redirect the user to payment gateway
-              let GatewayPageURL=(apiResponse.GatewayPageURL)
-            //   console.log(GatewayPageURL
-               res.json({ GatewayPageURL });
-                 console.log("Redirecting to: ", GatewayPageURL);  
+                let GatewayPageURL = (apiResponse.GatewayPageURL)
+                //   console.log(GatewayPageURL
+                res.json({ GatewayPageURL });
+
+                const finalOrder = {
+                    data,
+                    paidStatus: false,
+                    transectionId: tran_id
+                }
+
+                const result = orderCollection.insertOne(finalOrder);
+
+
+                // console.log("Redirecting to: ", GatewayPageURL);
             });
+
+            app.post("/payment/success/:tranId", async (req, res) => {
+                // console.log(req.params.tranId);
+                const result = await orderCollection.updateOne({ transectionId: req.params.tranId },
+                    {
+                        $set: {
+                            paidStatus: true,
+                        }
+                    }
+                );
+                    if(result.modifiedCount>0){
+                        res.redirect(`http://localhost:5173/payment/success/${req.params.tranId}`)
+                    }
+            })
+
+
         },)
-       
+
 
 
         // Blogs
@@ -252,28 +279,28 @@ async function run() {
             res.send(result);
         });
 
-      // student verification
-      app.get("/users/student/:email", verifyJwt, async (req, res) => {
-        const email = req.params.email;
-        console.log(email);
-        if (req.decoded.email !== email) {
-          res.send({ student: false });
-        }
-        const query = { email: email };
-        const user = await userCollection.findOne(query);
-        const result = { student: user?.Roll == "student" };
-        res.send(result);
-      });
+        // student verification
+        app.get("/users/student/:email", verifyJwt, async (req, res) => {
+            const email = req.params.email;
+            console.log(email);
+            if (req.decoded.email !== email) {
+                res.send({ student: false });
+            }
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const result = { student: user?.Roll == "student" };
+            res.send(result);
+        });
 
-    
-      // Delete user from db
-      app.delete("/users/DeleteUsers/:id", async (req, res) => {
-        const id = req.params.id;
-        console.log(id);
-        const query = { _id: new ObjectId(id) };
-        const result = await userCollection.deleteOne(query);
-        res.send(result);
-      });
+
+        // Delete user from db
+        app.delete("/users/DeleteUsers/:id", async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: new ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        });
 
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
