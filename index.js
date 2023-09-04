ffdf45c2f551e62ce8838774a9a62c92a0d0375e
Rashik <smarashik00@gmail.com>
2023-09-04 10:43:05 +0600

    first commit

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ app.use(express.json());
 
 // Ping Endpoint
 app.get("/", (req, res) => {
-  res.send("Spoken English Server is running");
+    res.send("Spoken English Server is running");
 });
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
@@ -24,106 +24,106 @@ const uri = `mongodb+srv://${process.env.DB_UserName}:${process.env.DB_Key}@clus
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
 });
 
 async function run() {
-  try {
-    //DB Collections
-    const blogsCollection = client.db("Spoken-English").collection("blogs");
+    try {
+        //DB Collections
+        const blogsCollection = client.db("Spoken-English").collection("blogs");
 
-    const userCollection = client.db("Spoken-English").collection("users");
+        const userCollection = client.db("Spoken-English").collection("users");
 
-    // APIs
+        // APIs
 
-    // Blogs
-    app.get("/blogs", async (req, res) => {
-      const result = await blogsCollection.find().toArray();
-      res.send(result);
-    });
+        // Blogs
+        app.get("/blogs", async (req, res) => {
+            const result = await blogsCollection.find().toArray();
+            res.send(result);
+        });
 
-    app.get("/blog/:id", async (req, res) => {
-      const blogId = req.params.id;
-      const query = { _id: new ObjectId(blogId) };
-      const result = await blogsCollection.findOne(query);
-      res.send(result);
-    });
+        app.get("/blog/:id", async (req, res) => {
+            const blogId = req.params.id;
+            const query = { _id: new ObjectId(blogId) };
+            const result = await blogsCollection.findOne(query);
+            res.send(result);
+        });
 
-    // save logged users
-    app.post("/AddUsers", async (req, res) => {
-      const user = req.body;
-      const query = { email: user.email };
-      const existingUser = await userCollection.findOne(query);
+        // save logged users
+        app.post("/AddUsers", async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email };
+            const existingUser = await userCollection.findOne(query);
 
-      if (existingUser) {
-        return res.send({ message: "user already exists" });
-      }
+            if (existingUser) {
+                return res.send({ message: "user already exists" });
+            }
 
-      const result = await userCollection.insertOne(user);
-      res.send(result);
-    });
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        });
 
-    // Get user collection
-    app.get("/GetUsers", async (req, res) => {
-      const result = await userCollection.find().toArray();
-      res.send(result);
-    });
+        // Get user collection
+        app.get("/GetUsers", async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        });
 
-    // make admin
-    app.patch("/users/admin/:id", async (req, res) => {
-      const id = req.params.id;
-      console.log(id);
-      const filter = { _id: new ObjectId(id) };
-      const updateDoc = {
-        $set: {
-          Roll: "admin",
-          disabled: true,
-        },
-      };
+        // make admin
+        app.patch("/users/admin/:id", async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    Roll: "admin",
+                    disabled: true,
+                },
+            };
 
-      const result = await userCollection.updateOne(filter, updateDoc);
-      res.send(result);
-    });
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        });
 
-    //make user to instructor
-    app.patch("/users/instructor/:id", async (req, res) => {
-      const id = req.params.id;
-      console.log(id);
-      const filter = { _id: new ObjectId(id) };
-      const updateDoc = {
-        $set: {
-          Roll: "Instructor",
-          InstructorDisabled: true,
-        },
-      };
+        //make user to instructor
+        app.patch("/users/instructor/:id", async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    Roll: "Instructor",
+                    InstructorDisabled: true,
+                },
+            };
 
-      const result = await userCollection.updateOne(filter, updateDoc);
-      res.send(result);
-    });
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        });
 
-    // Delete user from db
-    app.delete("/users/DeleteUsers/:id", async (req, res) => {
-      const id = req.params.id;
-      console.log(id);
-      const query = { _id: new ObjectId(id) };
-      const result = await userCollection.deleteOne(query);
-      res.send(result);
-    });
+        // Delete user from db
+        app.delete("/users/DeleteUsers/:id", async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: new ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        });
 
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log(
+            "Pinged your deployment. You successfully connected to MongoDB!"
+        );
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
